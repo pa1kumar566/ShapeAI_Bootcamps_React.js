7cce0bbc9bf5067142fa706bee488ea7b9b76835
Pavankumar <77559797+pa1kumar566@users.noreply.github.com>
2021-06-27 20:23:41 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer(){
+  return (
+    <footer>
+    <p>
+      Copyright by SHAPE AI {new Date().getFullYear()}
+    </p>
+    </footer>
+  );
+  
+}
+
+export default Footer;
---
+++ Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header(){
+  return (
+    <header>
+      <h1>
+        SHAPE AI BOOTCAMPS
+      </h1>
+    </header>
+  );
+  
+}
+
+export default Header;
